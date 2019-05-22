e2e9782050a7ec4367759f66e90e71d6dcb26bd5
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2019-05-22 12:25:08 +0200

    Add bash script

---
+++ build-run-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export TAG=springaad
+
+docker build \
+    --tag "${TAG}" \
+    .
+
+docker run \
+    -e "AAD_TENANT_ID=${AAD_TENANT_ID}" \
+    -e "AAD_CLIENT_ID=${AAD_CLIENT_ID}" \
+    -e "AAD_CLIENT_SECRET=${AAD_CLIENT_SECRET}" \
+    -e "AAD_GROUP=${AAD_GROUP}" \
+    -e "KEYVAULT_URI=${KEYVAULT_URI}" \
+    -p 8080:8080/tcp \
+    --interactive --tty --rm \
+    "${TAG}"
